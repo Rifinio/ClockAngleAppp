299b736eac9033a60f2b10fe09eee209e4fe5017
Adil Bougamza <adil.bougamza@gmail.com>
2015-04-19 01:33:57 +0100

    [feature] add an analogue clock and set handles

--- ClockApp/ViewController.m
+++ ClockApp/ViewController.m
@@ -8,15 +8,24 @@
 
 #import "ViewController.h"
 
-#define DEGREE_MIN                  6
+#define DEGREE_MIN                   6
 #define DEGREE_HOUR                 30
 #define DEGREE_ADVANCED_BY_MIN     0.5
 #define TOTAL_DEGREES              360
 
 
 @interface ViewController ()
+{
+    UIImageView* imageViewMinuteHandle;
+    UIImageView* imageViewHourHandle;
+}
+
+
+
 @property (weak, nonatomic) IBOutlet UIDatePicker *datePickerTime;
 @property (weak, nonatomic) IBOutlet UILabel *labelMessage;
+
+@property (weak, nonatomic) IBOutlet UIView *viewClock;
 
 @end
 
@@ -27,12 +36,25 @@
     [super viewDidLoad];
 	
     self.navigationItem.title =  @"Edit Alarm";
-    
+
     // set the local of the date picker to show a 24 h
     NSLocale *locale = [[NSLocale alloc] initWithLocaleIdentifier:@"da_DK"];
     [_datePickerTime setLocale:locale];
     
     [self setAngleLabelMessage];
+}
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [self setAngleLabelMessage];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
 }
 
 -(float) computeAngleBetweenHandlesForTime:(NSDate*)time
@@ -65,6 +87,9 @@
     // the exact hour's handle degree
     float hours_handle_degree = hour_degree + hour_advanced_degree;
     
+    // set Clock handles
+    [self setClockHandlesAtHour:hours_handle_degree andMinuteDegrees:minutes_handle_degree];
+    
     // the angle between the two handles
     angle = fabs(hours_handle_degree - minutes_handle_degree);
     
@@ -73,22 +98,56 @@
     
     return angle;
 }
-- (void)didReceiveMemoryWarning
+
+-(void) setClockHandlesAtHour:(float) hourDegrees andMinuteDegrees:(float) minuteDegrees
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+    float min_rad   = minuteDegrees * M_PI/180;
+    float hour_rad  = hourDegrees * M_PI/180;
+    
+    if (!imageViewMinuteHandle && !imageViewHourHandle)
+    {
+        [self addImageHandles];
+    }
+    else
+    {
+        [imageViewMinuteHandle removeFromSuperview]; imageViewMinuteHandle = nil;
+        [imageViewHourHandle removeFromSuperview]; imageViewHourHandle = nil;
+        
+        [self addImageHandles];
+    }
+    
+    imageViewMinuteHandle.transform    = CGAffineTransformMakeRotation(min_rad);
+    imageViewHourHandle.transform      = CGAffineTransformMakeRotation(hour_rad);
 }
 
+-(void) addImageHandles
+{
+    imageViewMinuteHandle = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 128, 128)];
+    imageViewMinuteHandle.image = [UIImage imageNamed:@"minutes.png"];
+    [_viewClock addSubview:imageViewMinuteHandle];
+    
+    imageViewHourHandle = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 128, 128)];
+    imageViewHourHandle.image = [UIImage imageNamed:@"hours.png"];
+    [_viewClock addSubview:imageViewHourHandle];
 
-- (IBAction)datePickerValueChanged:(id)sender {
-    [self setAngleLabelMessage];
 }
+
 
 -(void) setAngleLabelMessage
 {
     float angle = [self computeAngleBetweenHandlesForTime:_datePickerTime.date];
     _labelMessage.text = [NSString stringWithFormat:@"The smaller angle between\nclock hands is %.2f degrees", angle];
-
+    
 }
+
+#pragma mark IBActions
+
+- (IBAction)datePickerValueChanged:(id)sender
+{
+    [self setAngleLabelMessage];
+}
+
+
 
 @end
